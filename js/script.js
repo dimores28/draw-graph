5da9f41f2357b1bccaea516ed9143aa0c80b6e42
Dmytro <dmytroivanovichn@gmail.com>
2023-08-13 11:04:59 +0300

    authorization and tree rendering with the new library

--- js/script.js
+++ js/script.js
@@ -40,7 +40,6 @@ function getColor(postType) {
    }
 }
 
-
 (async ()=>{
    data = await makeRequest(1);
    const ctorage = JSON.parse(localStorage.getItem("nodes"));
@@ -66,13 +65,13 @@ function getColor(postType) {
       nodes.push(node);
    });
 
-   console.log(nodes);
+   // console.log(nodes);
    let index = 0;
    let actNdCent = 0;
    let compact = 0;
 
    const newNode = nodes.filter(el => el.newNode == true);
-   console.log(newNode);
+   // console.log(newNode);
    nodes = nodes.filter(el => el?.newNode != true);
 
    const chart = new d3.OrgChart()
@@ -170,3 +169,149 @@ function getColor(postType) {
    //chart.setUpToTheRootHighlighted("1").render().fit() //id mark root
    //chart.clearHighlighting(); clear mark
 })();
+
+
+function onTelegramAuth(user) {
+   if (user.id) {
+      console.log("Telegram user data",user);
+      const body = document.querySelector("body");
+      body.innerHTML = `
+         <main class="main">
+            <div class="chart-container"></div>
+         </main>
+      `;
+
+      (async ()=>{
+         data = await makeRequest(1);
+         const ctorage = JSON.parse(localStorage.getItem("nodes"));
+         let nodes = [];
+      
+         data.forEach((item) => {
+            let node = {};
+      
+            node.id = item.id;
+            node.parentId = item.parentPostId;
+            node.location = getLocation(item.positiion);
+            node.title = item.title;
+            node.description = item.description;
+            node.postType = item.postType;
+      
+            if (ctorage) {
+              const newNode = ctorage.find((elem) => elem.id === node.id);
+              if (!newNode) {
+                node.newNode = true;
+              }
+            }
+      
+            nodes.push(node);
+         });
+      
+         // console.log(nodes);
+         let index = 0;
+         let actNdCent = 0;
+         let compact = 0;
+      
+         const newNode = nodes.filter(el => el.newNode == true);
+         // console.log(newNode);
+         nodes = nodes.filter(el => el?.newNode != true);
+      
+         const chart = new d3.OrgChart()
+         .container(".chart-container")
+         .data(nodes)
+         .nodeHeight((d) => 150)
+         .nodeWidth((d) => { return 200;})
+         .childrenMargin((d) => 50)
+         .compactMarginBetween((d) => 50)
+         .compactMarginPair((d) => 100)
+         .siblingsMargin((d) => 25)
+         .buttonContent(({ node, state }) => {
+           return `<div style="px;color:#716E7B;border-radius:5px;padding:4px;font-size:10px;margin:auto auto;background-color:white;border: 1px solid #E4E2E9"> <span style="font-size:9px">${
+             node.children
+               ? `<i class="fas fa-angle-up"></i>`
+               : `<i class="fas fa-angle-down"></i>`
+           }</span> ${node.data._directSubordinates}  </div>`;
+         })
+         .linkUpdate(function (d, i, arr) {
+           d3.select(this)
+             .attr("stroke", (d) =>
+               d.data._upToTheRootHighlighted ? "#14760D" : "#2CAAE5"
+             )
+             .attr("stroke-width", (d) =>
+               d.data._upToTheRootHighlighted ? 15 : 1
+             );
+      
+           if (d.data._upToTheRootHighlighted) {
+             d3.select(this).raise();
+           }
+         })
+         .nodeContent(function (d, i, arr, state) {
+      
+           return `
+               <div class="card" style="background:${getColor(d.data.postType)};">
+                  <div class="card__body">
+                     <div class="card__title">
+                        <a href="#">
+                           ${d.data.title}
+                        </a>
+                     </div>
+                     <div class="card__content">
+                           ${d.data.description}
+                     </div>
+                  </div>
+               </div>
+            `;
+         })
+         .render();
+      
+         chart.expandAll();
+         chart.compact(!!(compact++ % 2)).render().fit();
+         // chart.setExpanded("2", false).render();
+      
+         // nodes.forEach(item => {
+         //    if(item.newNode){
+         //       chart.setExpanded(item.id, false).render();
+         //       item.newNode = false;
+         //    }
+         // })
+      
+      
+      
+         document.querySelector('#swap').addEventListener('click', function() {
+            chart.layout(["right","bottom","left","top"][index++%4]).render().fit();
+         });
+      
+         document.querySelector('#zoom-out').addEventListener('click', function() {
+            chart.zoomOut(); // -
+         });
+      
+         document.querySelector('#zoom-in').addEventListener('click', function() {
+            chart.zoomIn(); // +
+         });
+      
+         document.querySelector('#fullscreen').addEventListener('click', function() {
+            chart.fullscreen('body');
+         });
+      
+         document.querySelector('#show').addEventListener('click', function() {
+            nodes = [...nodes, ...newNode];
+            chart.data(nodes).render();
+            chart.expandAll();
+      
+             nodes.forEach(item => {
+               if(item.newNode){
+                  item.newNode = false;
+               }
+            });
+            localStorage.setItem("nodes", JSON.stringify(nodes));
+         });
+      
+         
+         localStorage.setItem("nodes", JSON.stringify(nodes));
+         
+         //chart.setHighlighted("1").render() //id mark
+         //chart.setUpToTheRootHighlighted("1").render().fit() //id mark root
+         //chart.clearHighlighting(); clear mark
+      })();
+
+   }
+}
